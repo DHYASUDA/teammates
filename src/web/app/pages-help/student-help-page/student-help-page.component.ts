1f1cc88e3c0e397ef39db12aa5e97d68ce1ef936
david <dhyasuda0@gmail.com>
2025-12-07 13:45:00 -0600

    reverted changes to submit final repo

--- src/web/app/pages-help/student-help-page/student-help-page.component.ts
+++ src/web/app/pages-help/student-help-page/student-help-page.component.ts
@@ -13,10 +13,4 @@ export class StudentHelpPageComponent {
 
   readonly supportEmail: string = environment.supportEmail;
 
-  scroll(anchor: string): void {
-    const el = document.getElementById(anchor);
-    if (el) {
-      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-  }
 }
